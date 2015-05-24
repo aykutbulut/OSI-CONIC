14150df61fce20f37c607582bba5ff30114f5cc5
Aykut Bulut <aykutblt@gmail.com>
2015-05-24 14:40:50 -0400

    coin-or buildtools added is used for building. It is buggy for now and does not seem  having a solution. Only solution I found was to upgrade autoconf which we can not due to BuildTools autoconf 2.59 requirement.

---
+++ get_dependencies.sh
@@ -0,0 +1,6 @@
+# buildtools
+svn co https://projects.coin-or.org/svn/BuildTools/stable/0.8 BuildTools
+# coinutils
+svn co https://projects.coin-or.org/svn/CoinUtils/stable/2.10/CoinUtils CoinUtils
+# get osi
+svn co https://projects.coin-or.org/svn/Osi/stable/0.107/Osi Osi
